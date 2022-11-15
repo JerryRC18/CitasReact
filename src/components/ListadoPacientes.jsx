f713c06ffb94cfcbbf673c668c6f1caef2ceb27d
Gerardo <jesusgrobles9@gmail.com>
2022-11-14 23:04:06 -0600

    Add project struct with vite, reactJS and tailwindcss

---
+++ src/components/ListadoPacientes.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ListadoPacientes = () => {
+  return (
+    <div className="w-1/2 lg:3/5">
+      <h2>Pacientes</h2>
+    </div>
+  )
+}
+
+export default ListadoPacientes
