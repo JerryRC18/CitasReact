d6304ecf73fee4b46a8c256043b960d40a67e0f0
Gerardo <jesusgrobles9@gmail.com>
2022-11-16 22:25:55 -0600

    Add dynamic form

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,24 @@
+import {useState} from "react";
 import Header from "./components/Header";
 import Formulario from "./components/Formulario";
 import ListadoPacientes from "./components/ListadoPacientes";
 
 function App() {
   
-  const numero = 18;
+  const [pacientes, setPacientes] = useState([]);
 
   return (
     <div className="container mx-auto mt-20">
       <Header/>
 
       <div className="mt-12 md:flex">
-        <Formulario/>
-        <ListadoPacientes/>
+        <Formulario
+          pacientes = {pacientes}
+          setPacientes = {setPacientes} 
+        />
+        <ListadoPacientes
+          pacientes = {pacientes}
+        />
       </div>  
       
     </div>
--- src/components/Paciente.jsx
+++ src/components/Paciente.jsx
@@ -1,27 +1,40 @@
 
 
-const Paciente = () => {
-  return (
-    <div>
-        <div className="m-5 bg-white shadow-md px-5 py-5 rounded-xl">
-        <p className="font-bold mb-3 text-gray-700 uppercase" >Nombre: {''}
-            <span className="font-normal normal-case">Hook</span>
-        </p>
-        <p className="font-bold mb-3 text-gray-700 uppercase" >Propietario: {''}
-            <span className="font-normal normal-case">Jesus</span>
-        </p>
-        <p className="font-bold mb-3 text-gray-700 uppercase" >Email: {''}
-            <span className="font-normal normal-case">correo@correo.com</span>
-        </p>
-        <p className="font-bold mb-3 text-gray-700 uppercase" >Fecha Alta: {''}
-            <span className="font-normal normal-case">10 Diciembte 2022</span>
-        </p>
-        <p className="font-bold mb-3 text-gray-700 uppercase" >Sintomas: {''}
-            <span className="font-normal normal-case">Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs.
-            The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero's De Finibus Bonorum et Malorum for use in a type specimen book.</span>
-        </p>
+const Paciente = ({paciente}) => {
+
+    const { nombre, propietario, email, fecha, sintomas } = paciente
+
+
+    return (
+        <div className="mx-5 my-10 bg-white shadow-md px-5 py-5 rounded-xl">
+          <p className="font-bold mb-3 text-gray-700 uppercase" >Nombre: {''}
+              <span className="font-normal normal-case">{nombre}</span>
+          </p>
+          <p className="font-bold mb-3 text-gray-700 uppercase" >Propietario: {''}
+              <span className="font-normal normal-case">{propietario}</span>
+          </p>
+          <p className="font-bold mb-3 text-gray-700 uppercase" >Email: {''}
+              <span className="font-normal normal-case"> {email} </span>
+          </p>
+          <p className="font-bold mb-3 text-gray-700 uppercase" >Fecha Alta: {''}
+              <span className="font-normal normal-case"> {fecha} </span>
+          </p>
+          <p className="font-bold mb-3 text-gray-700 uppercase" >Sintomas: {''}
+              <span className="font-normal normal-case"> {sintomas} </span>
+          </p>
+
+          <div className="flex justify-between mt-10" >
+            <button
+                type="button"
+                className="py-2 px-10 bg-indigo-600 hover:bg-indigo-700 text-white font-bold uppercase rounded-lg"
+            >Editar</button>
+            
+            <button
+                type="button"
+                className="py-2 px-10 bg-red-600 hover:bg-red-700 text-white font-bold uppercase rounded-lg"
+            >Eliminar</button>
+          </div>
       </div>
-    </div>
   )
 }
 
